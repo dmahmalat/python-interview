32082b50633ec483a2364078440715d9ca395300
Dany Mahmalat <dany.mahmalat@gmail.com>
2021-12-02 03:18:41 -0500

    Update __main__.py

--- interview/__main__.py
+++ interview/__main__.py
@@ -1,4 +1,9 @@
 # Entrypoint file when the package is run with the -m flag from the command line
-from . import interview_refactor
+from . import interview_refactor, interview_coding_functions
 
+print("RUNNING INTERVIEW REFACTOR:")
 interview_refactor.main()
+
+print("\n-----------------------------------")
+print("RUNNING INTERVIEW CODING FUNCTIONS:")
+interview_coding_functions.main()
