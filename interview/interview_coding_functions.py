14db23fb797eff7af08d5e3f9b938e38f061210d
Dany Mahmalat <dany.mahmalat@gmail.com>
2021-12-02 05:44:01 -0500

    Update interview_coding_functions.py

--- interview/interview_coding_functions.py
+++ interview/interview_coding_functions.py
@@ -1,8 +1,9 @@
+# Imports =============================================================================
 import unittest
 from dataclasses import dataclass
 from typing import List
 
-
+# Class definitions ===================================================================
 @dataclass
 class Car:
     id: str
@@ -10,25 +11,29 @@ class Car:
     model: str
     price: int
 
-    # add code here
+    # Override __repr__ to match the test case formatting
+    def __repr__(self):
+        key_value_list = [f"{key}={value}" for key, value in self.__dict__.items()]
+        return "{}".format(' '.join(key_value_list))
 
+# Extending the list pre-built object
 class CarsCollection(list):
-    def __init__(self, cars: List[Car]):
-        # add code here
+    def sort_cheapest_first(self) -> None:
+        self.sort(key=lambda c: c.price)
 
-    def sort_cheapest_first(self):
-        # add code here
+    def sort_most_expensive_first(self) -> None:
+        self.sort(key=lambda c: c.price, reverse=True)
 
-    def sort_most_expensive_first(self):
-        # add code here
+    # Do not alter the list while getting the correct items
+    def get_cheapest(self) -> Car:
+        sorted_list = sorted(self, key=lambda c: c.price)
+        return sorted_list[0]
 
-    def get_cheapest(self):
-        # add code here
+    def get_most_expensive(self) -> Car:
+        sorted_list = sorted(self, key=lambda c: c.price, reverse=True)
+        return sorted_list[0]
 
-    def get_most_expensive(self):
-        # add code here
-
-
+# Code left intact for validation =====================================================
 class TestCarsCollection(unittest.TestCase):
     tesla = Car(id="1", make="Tesla", model="Model 3", price=45000)
     civic = Car(id="2", make="Honda", model="Civic", price=22000)
@@ -55,5 +60,9 @@ class TestCarsCollection(unittest.TestCase):
         self.assertEqual(first=self.civic.__repr__(), second="id=2 make=Honda model=Civic price=22000")
 
 
+def main():
+    # Context module required to discover the correct TestCase classes
+    unittest.main(module=__name__, exit=False)
+
 if __name__ == "__main__":
-    unittest.main()
+    main()
